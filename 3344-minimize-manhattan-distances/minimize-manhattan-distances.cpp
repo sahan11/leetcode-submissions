f9a1690f16ba26c0203cd9a1f1e177a98f1b4f16
sahan11 <112235945+sahan11@users.noreply.github.com>
2024-07-03 22:29:44 +0530

    Time: 242 ms (66.21%) | Memory: 122 MB (67.04%) - LeetSync

--- 3344-minimize-manhattan-distances/minimize-manhattan-distances.cpp
+++ 3344-minimize-manhattan-distances/minimize-manhattan-distances.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     int minimumDistance(vector<vector<int>>& points) {
-        vector<pair<int, int>> sum, diff;
-        int n = points.size();
-        for(int i = 0;i<points.size();i++) {
-            sum.push_back({points[i][0] + points[i][1], i});
-            diff.push_back({points[i][0] - points[i][1], i});
+        vector<int> ans = maxDistance(points);
+        return min(maxDistance(points, ans[1])[0], maxDistance(points, ans[2])[0]);
+    }
+
+    vector<int> maxDistance(vector<vector<int>> &ps, int skip = -1) {
+        int n = ps.size();
+        vector<int> sum = {INT_MAX, INT_MIN};
+        vector<int> dif = {INT_MAX, INT_MIN};
+        vector<int> sum_i = {0, 0};
+        vector<int> dif_i = {0, 0};
+
+        for(int i = 0;i<n;i++) {
+            if(i == skip) continue;
+            if(sum[0] > ps[i][0] + ps[i][1]) {
+                sum[0] = ps[i][0] + ps[i][1];
+                sum_i[0] = i;
+            }
+            if(sum[1] < ps[i][0] + ps[i][1]) {
+                sum[1] = ps[i][0] + ps[i][1];
+                sum_i[1] = i;
+            }
+            if(dif[1] < ps[i][0] - ps[i][1]) {
+                dif[1] = ps[i][0] - ps[i][1];
+                dif_i[1] = i;
+            }
+            if(dif[0] > ps[i][0] - ps[i][1]) {
+                dif[0] = ps[i][0] - ps[i][1];
+                dif_i[0] = i;
+            }
+            
         }
 
-        sort(sum.begin(), sum.end());
-        sort(diff.begin(), diff.end());
+        if(sum[1] - sum[0] > dif[1] - dif[0]) {
+            return {sum[1] - sum[0], sum_i[0], sum_i[1]};
 
-        int ans = INT_MAX;
-        for(int i = 0 ; i < n ; i++) {
-            
-            int left = 0;
-            int right = n - 1;
-            
-            // move left and right pointer if it is pointing at ith point
-            if(sum[left].second == i) left++;
-            if(sum[right].second == i) right--;
-            
-            // get maximum difference between two different values in the sum array
-            int sumVal = sum[right].first - sum[left].first;
-            
-            left = 0;
-            right = n - 1;
-            
-            // move left and right pointer if it is pointing at ith point
-            if(diff[left].second == i) left++;
-            if(diff[right].second == i) right--;
-            
-            // get maximum difference between two different values in the sum array
-            int diffVal = diff[right].first - diff[left].first;
-            
-            // update ans accordingly
-            ans = min(ans, max(sumVal, diffVal));
         }
-                
-        return ans;
+        else {
+            return {dif[1] - dif[0], dif_i[0], dif_i[1]};
+        }
     }
 };
